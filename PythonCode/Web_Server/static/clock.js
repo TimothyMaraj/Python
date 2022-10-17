104d3e6488402b3008a3634d1690324b03c07bc3
TimothyMaraj <timxmaraj@gmail.com>
2022-10-17 12:11:17 -0500

    web stuff

---
+++ PythonCode/Web_Server/static/clock.js
@@ -0,0 +1,32 @@
+function TimeRender() {
+
+    var currentDate = new Date(); 
+    var month = currentDate.getMonth(); 
+    var 
+    
+    var currentTime = new Date(); 
+    var hour = currentTime.getHours(); 
+    var minutes = currentTime.getMinutes(); 
+    var sec = currentTime.getSeconds(); 
+  
+    if(hour > 12)
+    {
+        hour-=12;
+    }
+
+    if(sec < 10)
+    {
+        sec = "0"+ sec; 
+    }
+    if(minutes < 10)
+    {
+        minutes = "0"+ minutes; 
+    }
+
+
+    var clock = document.getElementById("clockDisplay");
+    clock.textContent = hour + minutes + sec; 
+    clock.innerText = hour +":"+ minutes+ ":" + sec; 
+    setTimeout("TimeRender()",1000);
+  }
+  TimeRender();
---
+++ PythonCode/Web_Server/web_server.py
@@ -0,0 +1,50 @@
+"""
+
+TODO: 
+- check out here for dynamics 
+- https://github.com/Absolute-Tinkerer/Clock-of-Clocks
+
+
+- Look into templating with {%%} for html templates
+- look into 
+
+- https://stackoverflow.com/questions/53111362/fun-clock-streaming-text-with-python-and-flask
+- mentions the video streaming in this link  
+"""
+
+
+import os
+from datetime import datetime
+import random
+from flask import Flask, render_template, request, Response
+app = Flask(__name__)
+
+
+
+
+@app.route("/home")
+def home():
+    numone = random.randint(1,20)
+    numtwo = random.randint(1,20)
+    return render_template('home.html',random_num_one = numone,random_num_two = numtwo)
+
+
+@app.route("/about")
+def about():
+    return "<h1>Flask is working</h1>"    
+
+@app.route("/EasterEgg")
+def easter_egg(): 
+    return render_template('EasterEgg.html')
+
+@app.route("/")
+@app.route('/time')
+def time_feed():
+    #now = datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
+    return render_template('time.html') 
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0')
+    app.debug = True
+
+    #port=random.randrange(1,8080,1)
